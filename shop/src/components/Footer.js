bebc1a8d83af4b342fe3eeb22462587d29bcd5f1
kuror0lucifer <kovalishin.2014@mail.ru>
2023-10-18 21:17:28 +0200

    header & footer

---
+++ shop/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div>
+        <span>Все права защищены &copy; </span>
+      </div>
+    </footer>
+  );
+}
